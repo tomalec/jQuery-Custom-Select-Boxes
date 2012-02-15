fb427dd27e3da44c1f38c1d32648428f92d7328b
George T. Walters II <walterg2@gmail.com>
2012-02-15 13:55:08 -0500

    Adding Custom Select Script

---
+++ js/jquery.custom-select.js
@@ -0,0 +1,31 @@
+(function($) {
+	$.fn.extend({
+		customSelect: function(options) {
+			return this.each(function() {
+				var currentSelected = $(this).find(':selected');
+				$(this).after('<span class="customSelectBox"><span class="customSelectBoxInner">' + currentSelected.text() + '</span></span>').css({
+					"position": 'absolute',
+					"opacity": 0,
+					"fontSize": $(this).next().css('font-size'),
+					"filter":"progid:DXImageTransform.Microsoft.Alpha(Opacity=0)"
+				});
+				var selectBoxSpan = $(this).next();
+				var selectBoxWidth = parseInt($(this).width()) - parseInt(selectBoxSpan.css('padding-left')) - parseInt(selectBoxSpan.css('padding-right'));
+				var selectBoxSpanInner = selectBoxSpan.find(':first-child');
+				selectBoxSpan.css({
+					display: 'inline-block'
+				});
+				selectBoxSpanInner.css({
+					width: selectBoxWidth,
+					display: 'inline-block'
+				});
+				var selectBoxHeight = parseInt(selectBoxSpan.height()) + parseInt(selectBoxSpan.css('padding-top')) + parseInt(selectBoxSpan.css('padding-bottom'));
+				$(this).height(selectBoxHeight).change(function() {
+					// selectBoxSpanInner.text($(this).val()).parent().addClass('changed');   This was not ideal
+					selectBoxSpanInner.text($(this).find(':selected').text()).parent().addClass('changed');
+					// Thanks to Juarez Filho & PaddyMurphy
+				});
+			});
+		}
+	});
+})(jQuery);
